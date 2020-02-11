6d8958098cf60c85a50bdec2dcdb9851f1181bdf
ifotn <rich@infrontofthenet.com>
2020-02-11 08:57:59 -0500

    l6/1 - add food

--- controllers/foods.js
+++ controllers/foods.js
@@ -23,6 +23,29 @@ router.get('/', (req, res, next) => {
     })
 })
 
+// GET /foods/add -> show blank add food form
+router.get('/add', (req, res, next) => {
+    // load the add view we are about to create
+    res.render('foods/add')
+})
+
+// POST /foods/add -> process form submission
+router.post('/add', (req, res, next) => {
+    // create a new document in the foods collection using the Food model, we'll get an error or new food document back
+    Food.create({
+        name: req.body.name,
+        country: req.body.country
+    }, function(err, newFood) {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        else {
+            // load the updated foods index
+            res.redirect('/foods');
+        }
+    })
+})
 
 // make the controller public
 module.exports = router
