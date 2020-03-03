d5ac512acf9f3bff1844f509c5c6f7445d00f862
ifotn <rich@infrontofthenet.com>
2020-03-03 10:48:42 -0500

    l8 done

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,6 @@
 // create a json object of global variables
 module.exports = {
-    'db': 'mongodb+srv://comp2106:iamhungry2020@cluster0-pijii.mongodb.net/globalFood?retryWrites=true&w=majority',
+    'db': 'mongodb+srv://comp2106:@cluster0-pijii.mongodb.net/globalFood?retryWrites=true&w=majority',
     'ids': {
         'google': {
             'clientID': '343030349236-07srgtjgr6k334ka6vj7fm3d9nf5sgfq.apps.googleusercontent.com',
